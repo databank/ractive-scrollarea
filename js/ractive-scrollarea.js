1f4c53a2dac2f3c0a087a6694f37d2ca1ec5aa68
adrianpraja <adrian@databank.ro>
2017-07-21 10:14:09 -0700

    try quickfix

--- js/ractive-scrollarea.js
+++ js/ractive-scrollarea.js
@@ -22,6 +22,7 @@ Ractive.components.scrollarea = Ractive.extend({
 
 		if (inner.height < outer.height) {
 			this.set('scrollbar.height', 0)
+			this.set('scrollbar.top', 0 )    // bugfix: when area shrinks and scrollbar no longer needed, reset scroll to top 
 			return false;
 		}
 
